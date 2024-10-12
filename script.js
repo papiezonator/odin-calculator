1e6073a63dd8562ba3410e1e78a6192b61c5e524
papiezonator <52249311+papiezonator@users.noreply.github.com>
2024-10-12 19:22:37 +0200

    added few DOM variables to get the number

--- script.js
+++ script.js
@@ -13,3 +13,11 @@ function multiply(a,b){
 function divide(a,b){
     return a/b
 }
+
+resultSpace = document.querySelector(".result");
+
+
+one = parseInt(document.querySelector(".number-one").textContent);
+two = parseInt(document.querySelector(".number-two").textContent);
+three = parseInt(document.querySelector(".number-three").textContent);
+
