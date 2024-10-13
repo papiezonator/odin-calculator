e23a717ad8ce355a7105e43160a953d285003c0a
papiezonator <52249311+papiezonator@users.noreply.github.com>
2024-10-13 15:50:23 +0200

    modified the structure, added variables and a function recognizing given operator

--- script.js
+++ script.js
@@ -14,17 +14,47 @@ function divide(a,b){
     return a/b
 }
 
+function operate(first, op, second){
+    if(op == '+'){
+        return add(first, second);
+    }
+
+}
+
 resultSpace = document.querySelector(".result");
 
 let btn = document.querySelectorAll(".btn");
 
+let operatorBtn = document.querySelectorAll(".operator");
 
+let equals = document.querySelector(".equals");
+
+let firstNumber
+let secondNumber
+let operatorValue
+let displayValue
 
 btn.forEach(button => {
     button.addEventListener("click", () => {
-        console.log(parseInt(button.textContent));
-    })
-})
+        if(resultSpace.innerText == 0){
+            resultSpace.innerText = "";
+        }
+        console.log(button.textContent);
+        //let test = document.createTextNode(button.textContent);
+        
+        resultSpace.innerText = resultSpace.innerText + button.textContent;
+        displayValue = resultSpace.innerText;
+        //resultSpace.appendChild(test);
+    });
+});
+
+operatorBtn.forEach(button => {
+    button.addEventListener("click", () => {
+        operatorValue = button.innerText;
+    });
+});
+
+
 
 
 
