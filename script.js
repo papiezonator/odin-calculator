9451c3f8d5c2ca0358411f0646244520c509acfa
papiezonator <52249311+papiezonator@users.noreply.github.com>
2024-10-14 22:13:25 +0200

    made operate into a switch statement based function, added very basic logic

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
-function add(a, b){
-    return a+b;
-}
+//display tylko pokazuje, rzeczywista wartosc jest trzymana jako array, operator sluzy do oddzielenia, jesli operator jest wiecej niz 1 to wykonuje 
+//funkcje zadeklarowan przez 1 op
+
+resultSpace = document.querySelector(".result");
+
+let btn = document.querySelectorAll(".btn");
+
+let operatorBtn = document.querySelectorAll(".operator");
+
+let equals = document.querySelector(".equals");
+
+let firstNumber
+let secondNumber
+let operatorValue
+
+let displayValue
+
+
+/*function add(a, b){
+
+        let sum = parseInt(a)+parseInt(b);
+    console.log(sum);
+    
+}*/
 
 function substract(a,b){
     return a-b;
@@ -14,47 +35,86 @@ function divide(a,b){
     return a/b
 }
 
-function operate(first, op, second){
-    if(op == '+'){
-        return add(first, second);
+function operate(first, operator, second){
+    switch(operator){
+        case '+':
+            
+            let sum = parseInt(first) + parseInt(second);
+            firstNumber = sum;
+            resultSpace.innerText = sum;
+            console.log(sum);
+            return;
+        case '-':
+            let subs = parseInt(first) - parseInt(second);
+            firstNumber = subs;
+            resultSpace.innerText = subs;
+            console.log(subs);
+            return;
+        case 'x':
+            let product = parseInt(first) * parseInt(second);
+            firstNumber = product;
+            resultSpace.innerText = product;
+            console.log(product);
+            return;
+        case '/':
+            let quotient = parseInt(first) / parseInt(second);
+            firstNumber = quotient;
+            resultSpace.innerText = quotient;
+            console.log(quotient);   
+            return;
     }
+    
 
 }
 
-resultSpace = document.querySelector(".result");
 
-let btn = document.querySelectorAll(".btn");
-
-let operatorBtn = document.querySelectorAll(".operator");
-
-let equals = document.querySelector(".equals");
-
-let firstNumber
-let secondNumber
-let operatorValue
-let displayValue
 
 btn.forEach(button => {
     button.addEventListener("click", () => {
         if(resultSpace.innerText == 0){
             resultSpace.innerText = "";
         }
-        console.log(button.textContent);
-        //let test = document.createTextNode(button.textContent);
-        
-        resultSpace.innerText = resultSpace.innerText + button.textContent;
+        //resultSpace.innerText += button.textContent;
+
         displayValue = resultSpace.innerText;
-        //resultSpace.appendChild(test);
+        if(operatorValue == null){
+            firstNumber = resultSpace.innerText += button.textContent
+        } else if (operatorValue != null){
+            
+            secondNumber = resultSpace.innerText += button.textContent
+            
+        }
     });
 });
 
 operatorBtn.forEach(button => {
     button.addEventListener("click", () => {
         operatorValue = button.innerText;
+
+        resultSpace.innerText = "";
+        //resultSpace.innerText = "";
+        
+       
+
     });
 });
 
-
+equals.addEventListener("click", () => {
+    if(operatorValue == "+"){
+        operate(firstNumber, '+' ,secondNumber);
+    }
+    if(operatorValue == "-"){
+        operate(firstNumber, '-' ,secondNumber);
+    }
+    if(operatorValue == "/"){
+        operate(firstNumber, '/' ,secondNumber);
+    }
+    if(operatorValue == "x"){
+        operate(firstNumber, 'x' ,secondNumber);
+    }
+    
+    
+})
 
 
 
